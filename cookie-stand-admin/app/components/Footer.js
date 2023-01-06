ceeaa23a65b6a23c7265af75231e4ccf1c08d583
ihababbas <ihababbas26@gmail.com>
2023-01-06 17:54:22 +0200

    done

--- cookie-stand-admin/app/components/Footer.js
+++ cookie-stand-admin/app/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer(){
     return(
-        <footer className='py-6 text-center  bg-green-900'>
-            <p className="text-black font-center light text-mb-3">Copy Right 2023 by Ihab Abbas</p>
+        <footer className='py-6 text-center  bg-green-900 dark:bg-slate-900 dark:bg-black dark:text-white'>
+            <p className="text-black font-center light text-mb-3  dark:text-slate-400">Copy Right 2023 by Ihab Abbas</p>
         </footer>
     )
 }
--- cookie-stand-admin/app/layout.tsx
+++ cookie-stand-admin/app/layout.tsx
@@ -1,25 +1,26 @@
+'use client';
 import './globals.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import ThemeWrapper from './contexts/theme'
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <ThemeWrapper>
       <body>
         <Header />
-        <main>
+        <main className="bg-white">
           {children}
         </main>
         <Footer />
       </body>
+      </ThemeWrapper>
     </html>
   )
 }
