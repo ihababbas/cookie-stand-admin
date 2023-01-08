5b0dd05a54e7ec797f1505a81d594a69745f5afa
ihababbas <ihababbas26@gmail.com>
2023-01-08 13:09:53 +0200

    done

--- cookie-stand-admin/app/page.tsx
+++ cookie-stand-admin/app/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { Inter } from '@next/font/google'
 
 import Form from './components/Form'
 
+import LoginForm from './components/LoginForm'
+import {useContext} from 'react';
+import { AuthContext } from './contexts/auth';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
- <div>
-   <Form />
 
- </div>
+  const {tokens} = useContext(AuthContext);
+  return (
+    <>
+     {tokens ? <> <Form /></> : <LoginForm />}
+       
+       </> 
+    
   )
 }
